41e815292a2f2d94c5f3c44909687986193893cd
Rafael Arnosti <r.brucknerarnosti@reply.eu>
2019-12-18 15:39:07 +0000

    powershell-update

--- Setup-ssms.ps1
+++ Setup-ssms.ps1
@@ -1,24 +1,25 @@
 # Set file and folder path for SSMS installer .exe
-$folderpath="c:\windows\temp"
-$filepath="$folderpath\SSMS-Setup-ENU.exe"
+########$folderpath="c:\windows\temp"
+########$filepath="$folderpath\SSMS-Setup-ENU.exe"
  
 #If SSMS not present, download
-if (!(Test-Path $filepath)){
-write-host "Downloading latest SSMS..."
-$URL = "https://aka.ms/ssmsfullsetup"
-$clnt = New-Object System.Net.WebClient
-$clnt.DownloadFile($url,$filepath)
-Write-Host "SSMS installer download completed" -ForegroundColor Green
+########if (!(Test-Path $filepath)){
+########write-host "Downloading latest SSMS..."
+########$URL = "https://aka.ms/ssmsfullsetup"
+########$clnt = New-Object System.Net.WebClient
+########$clnt.DownloadFile($url,$filepath)
+########Write-Host "SSMS installer download completed" -ForegroundColor Green
  
-}
-else {
+########}
+########else {
  
-write-host "Found setup file, moving on to install..."
-}
+########write-host "Found setup file, moving on to install..."
+########}
  
 # start the SSMS installer
-write-host "Installing SSMS...." -nonewline
-$Parms = " /Install /Quiet /Norestart /Logs log.txt"
-$Prms = $Parms.Split(" ")
-& "$filepath" $Prms | Out-Null
-Write-Host "SSMS installation complete" -ForegroundColor Green
+########write-host "Installing SSMS...." -nonewline
+########$Parms = " /Install /Quiet /Norestart /Logs log.txt"
+########$Prms = $Parms.Split(" ")
+########& "$filepath" $Prms | Out-Null
+########Write-Host "SSMS installation complete" -ForegroundColor Green
+write-host "Installing SQL Cluster 2019"
